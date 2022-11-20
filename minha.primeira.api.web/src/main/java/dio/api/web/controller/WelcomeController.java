4585d28e97fbb03d3cc07045639cc3e8dce92c4a
LcSilva0702 <lcsilva0702@gmail.com>
2022-11-20 14:14:21 -0300

    todos os testes devidamente feitos

---
+++ minha.primeira.api.web/src/main/java/dio/api/web/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package dio.api.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController{
+    @GetMapping
+    public String welcome(){
+        return "Bem-vindo a minha primeira API";
+    }
+
+}
